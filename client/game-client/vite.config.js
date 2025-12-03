0290bfbb60ec90785fdf63a2f42346b39b3ffb7d
ClaytonWas <claywasmund@gmail.com>
2025-12-03 04:16:54 -0500

    adding vitest

--- client/game-client/vite.config.js
+++ client/game-client/vite.config.js
@@ -14,7 +14,10 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/test/setup.js'
+    setupFiles: './src/test/setup.js',
+    coverage: {
+      provider: 'v8'
+    }
   }
 })
 
